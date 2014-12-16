8bdf25a007f362ae3b763482784a0bacee769556
returnfirst <returnfirst@gmail.com>
2014-12-17 08:01:54 +0900

    Lesson1 - Data Type
    How to use .replace and .append

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,9 @@
-$("#main").append("정열");
+//$("#main").append("정열");
+var email = "returnfirst@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+console.log(email);
+console.log(newEmail);
+var awesomeThoughts = "My name is 정열 and I am AWESOME!";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
