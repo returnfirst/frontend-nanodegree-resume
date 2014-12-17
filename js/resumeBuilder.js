b2ef16a9ece74b15d52a0473c05627a7a465aed8
returnfirst <returnfirst@gmail.com>
2014-12-17 15:16:39 +0900

    JSON
    How to create JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,80 @@
-//$("#main").append("정열");
-var email = "returnfirst@udacity.com";
-var newEmail = email.replace("udacity", "gmail");
-console.log(email);
-console.log(newEmail);
-var awesomeThoughts = "My name is 정열 and I am AWESOME!";
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+var bio = {
+  "name": "JY Jin",
+  "role": "Web based 3D Application Developer",
+  "contacts": {
+    "email": "returnfirst@gmail.com",
+    "github": "returnfirst",
+    "location": "Seoul"
+  },
+  "welcomeMessage": "Hi, everyone. My name is JY Jin. Nice meet you.",
+  "skills": ["awesomeness", "programming", "learning", "JS", "WebGL"],
+  "bioPic": "images/fry.jpg"
+}
+
+var education = {
+  "schools": [{
+    "name": "KS univ.",
+    "location": "Busan",
+    "majors": "Computer Engineering",
+    "date": 1999,
+    "url": "http://www.google.com"
+  }, {
+    "name": "DD mid.",
+    "location": "Busan",
+    "majors": "Liberal arts track",
+    "date": 1991,
+    "url": "http://www.google.com"
+  }],
+  "onlineCourses": [{
+    "title": "JavaScript Basics",
+    "school": "Udacity",
+    "dates": 2014,
+    "url": "http://www.udacity.com/course/ud804"
+  }]
+}
+
+var work = {
+  "jobs": [{
+    "employer": "Intelli Korea Inc.",
+    "title": "Senior Researcher",
+    "location": "Seoul",
+    "dates": "2014 ~ future",
+    "description": "3D Printing R&D Project Team, Software R&D"
+  }, {
+    "employer": "Real D Square co., Ltd.",
+    "title": "Senior Researcher",
+    "location": "Seoul",
+    "dates": "2012 ~ 2014",
+    "description": "Government R&D Project Team Manager, Software R&D"
+  }, {
+    "employer": "CoDiC communications Inc.",
+    "title": "Researcher",
+    "location": "Seoul",
+    "dates": "2003 ~ 2011",
+    "description": "Software research and develop"
+  }]
+}
+
+var projects = {
+  "projects": [{
+    "title": "DDD Project",
+    "dates": "2012 ~ 2014",
+    "description": "Good job",
+    "images": "images/fry.jpg"
+  }, {
+    "title": "CCC Project",
+    "dates": "2009 ~ 2012",
+    "description": "Good job",
+    "images": "images/fry.jpg"
+  }, {
+    "title": "BBB Project",
+    "dates": "2005 ~ 2008",
+    "description": "Good job",
+    "images": "images/fry.jpg"
+  }, {
+    "title": "AAA Project",
+    "dates": "2003 ~ 2005",
+    "description": "Good job",
+    "images": "images/fry.jpg"
+  }, ]
+}
